68546a7ddf1bb7c5cc95bbd8626b3519c55357c7
juan quintero <juanestquintero@gmail.com>
2024-02-23 08:35:18 -0500

    feat(api): orders and accounts in memory

--- beer_api/beer_app/views.py
+++ beer_api/beer_app/views.py
@@ -5,8 +5,18 @@ from .models import Beer
 from .serializers import BeerSerializer
 
 
+# Save orders and accounts in memory for simplicity
+orders = []
+accounts = {"Friend1": 0, "Friend2": 0, "Friend3": 0}
+
+
 @api_view(['GET'])
 def list_beers(request):
     beers = Beer.objects.all()
     serializer = BeerSerializer(beers, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_account(request):
+    return Response(accounts)
