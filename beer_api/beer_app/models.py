114746f093587fe2706e3526daedbb9a6cd96940
juan quintero <juanestquintero@gmail.com>
2024-02-23 08:26:09 -0500

    chore(api): beer model and serializer

--- beer_api/beer_app/models.py
+++ beer_api/beer_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Beer(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return str(self.name)
