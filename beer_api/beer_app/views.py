4b4a9e6f566ad4aef78fe339a088c20d8e1ddca2
juan quintero <juanestquintero@gmail.com>
2024-02-23 08:27:44 -0500

    chore(api): get beer list

--- beer_api/beer_app/views.py
+++ beer_api/beer_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Beer
+from .serializers import BeerSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def list_beers(request):
+    beers = Beer.objects.all()
+    serializer = BeerSerializer(beers, many=True)
+    return Response(serializer.data)
