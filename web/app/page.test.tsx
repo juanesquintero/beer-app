078afcd3acbba39e54835f4a110daf60a300c769
juan quintero <juanestquintero@gmail.com>
2024-03-06 12:08:55 -0500

    test(web):  page fetch mocked data

--- web/app/page.test.tsx
+++ web/app/page.test.tsx
@@ -1,34 +1,36 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import Home from './page';
-import { fetchData } from '@/shared/helpers';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "./page";
+import { fetchData } from "@/shared/helpers";
 
 // Mock the fetchData function
-jest.mock('../shared/helpers', () => ({
+jest.mock("../shared/helpers", () => ({
   fetchData: jest.fn(),
 }));
 
-describe('Home', () => {
-  it('renders without crashing', () => {
+const friendsMock = [
+  { owed: 5.99, name: "John Doe" },
+  { owed: 10.5, name: "Jane Doe" },
+];
+
+describe("Home", () => {
+  it("renders without crashing", () => {
     render(<Home />);
-    expect(screen.getByText('Payment Interface')).toBeInTheDocument();
+    expect(screen.getByText("Payment Interface")).toBeInTheDocument();
   });
 
-  it('fetches and displays a list of friends', async () => {
+  it("fetches and displays a list of friends", async () => {
     // Mock the API call
-    fetchData.mockResolvedValue([
-      { id: 1, name: 'John Doe' },
-      { id: 2, name: 'Jane Doe' },
-    ]);
+    fetchData.mockResolvedValue(friendsMock);
 
     render(<Home />);
 
     // Wait for the fetchData to be called and state to update
     await waitFor(() => {
-      expect(fetchData).toHaveBeenCalledWith('/friends/');
-      expect(screen.getByText('John Doe')).toBeInTheDocument();
-      expect(screen.getByText('Jane Doe')).toBeInTheDocument();
+      expect(fetchData).toHaveBeenCalledWith("/friends/");
+      expect(screen.getByTestId("John Doe")).toBeInTheDocument();
+      expect(screen.getByTestId("Jane Doe")).toBeInTheDocument();
     });
   });
 });
